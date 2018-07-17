b9508376e9bdd63511e7037b26ceeeb835adaaa8
BADAWEY Mohammed Ext OBS/CSO <dzkl1918@orange.com>
2018-07-17 21:35:17 +0200

    modifying .environ directly

--- set-db-env-variables.py
+++ set-db-env-variables.py
@@ -6,6 +6,6 @@ vcap = json.loads(os.getenv('VCAP_SERVICES'))
 cred = vcap['p-mysql'][0]['credentials']
 
 print "loaded cred is " , cred
-os.putenv('JDBC_URL',cred['uri'])
-os.putenv('JDBC_USERNAME',cred['username'])
-os.putenv('JDBC_PASSWORD',cred['password'])
+os.environ['JDBC_URL'] = cred['uri']
+os.environ['JDBC_USERNAME'] = cred['username']
+os.environ['JDBC_PASSWORD'] = cred['password']
