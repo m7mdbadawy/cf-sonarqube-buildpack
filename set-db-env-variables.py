8c9b4175de43f7f5aae48216f6571a97517b9c44
BADAWEY Mohammed Ext OBS/CSO <dzkl1918@orange.com>
2018-07-17 21:48:14 +0200

    running perl from python

--- set-db-env-variables.py
+++ set-db-env-variables.py
@@ -9,3 +9,5 @@ print "loaded cred is " , cred
 os.environ['JDBC_URL'] = cred['uri']
 os.environ['JDBC_USERNAME'] = cred['username']
 os.environ['JDBC_PASSWORD'] = cred['password']
+
+subprocess.call("""perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' ./sonar.properties > ./sonar_replaced.properties""", shell=True)
