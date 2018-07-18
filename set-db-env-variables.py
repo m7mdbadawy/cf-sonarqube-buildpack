bc8e10a0b5e206111667389375618e63719d267b
BADAWEY Mohammed Ext OBS/CSO <dzkl1918@orange.com>
2018-07-18 11:55:19 +0200

    updating jdbc url

--- set-db-env-variables.py
+++ set-db-env-variables.py
@@ -7,7 +7,8 @@ vcap = json.loads(os.getenv('VCAP_SERVICES'))
 cred = vcap['p-mysql'][0]['credentials']
 
 print "loaded cred is " , cred
-os.environ['JDBC_URL'] = cred['jdbcUrl']
+os.environ['JDBC_URL'] = cred['jdbcUrl'] + "&useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true
+"
 os.environ['JDBC_USERNAME'] = cred['username']
 os.environ['JDBC_PASSWORD'] = cred['password']
 
