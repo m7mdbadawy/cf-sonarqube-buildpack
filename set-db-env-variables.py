d4857c7ca1451f51d07b4878861bf5e3c7b63293
BADAWEY Mohammed Ext OBS/CSO <dzkl1918@orange.com>
2018-07-18 12:18:05 +0200

    updating jdbc url

--- set-db-env-variables.py
+++ set-db-env-variables.py
@@ -7,6 +7,6 @@ vcap = json.loads(os.getenv('VCAP_SERVICES'))
 cred = vcap['postgres-cluster'][0]['credentials']
 
 print "loaded cred is " , cred
-os.environ['JDBC_URL'] = "jdbc:" + cred['uri']
+os.environ['JDBC_URL'] = "jdbc:" + cred['uri'].replace('postgres','postgresql')
 
 subprocess.call("""perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' ./sonar.properties > ./sonar_replaced.properties""", shell=True)
