6f3d93488835e25e84afbbd72cbbb56b79e6f956
BADAWEY Mohammed Ext OBS/CSO <dzkl1918@orange.com>
2018-07-18 12:08:54 +0200

    updating jdbc url

--- set-db-env-variables.py
+++ set-db-env-variables.py
@@ -4,11 +4,9 @@ import subprocess
 
 vcap = json.loads(os.getenv('VCAP_SERVICES'))
 
-cred = vcap['p-mysql'][0]['credentials']
+cred = vcap['postgres-cluster'][0]['credentials']
 
 print "loaded cred is " , cred
-os.environ['JDBC_URL'] = cred['jdbcUrl'] + "&useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true"
-os.environ['JDBC_USERNAME'] = cred['username']
-os.environ['JDBC_PASSWORD'] = cred['password']
+os.environ['JDBC_URL'] = "jdbc:" + cred['uri']
 
 subprocess.call("""perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' ./sonar.properties > ./sonar_replaced.properties""", shell=True)
